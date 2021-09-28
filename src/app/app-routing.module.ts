8f9076071b2774fd72227694cc81eb539229ac82
christophe <christophegueroult@gmail.com>
2021-09-28 17:04:05 +0200

    routes clients, orders and page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PageForgotComponent } from './login/pages/page-forgot/page-forgot.compo
 import { PageResetComponent } from './login/pages/page-reset/page-reset.component';
 import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
 import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
+import { PageListOrdersComponent } from './orders/pages/page-list-orders/page-list-orders.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },

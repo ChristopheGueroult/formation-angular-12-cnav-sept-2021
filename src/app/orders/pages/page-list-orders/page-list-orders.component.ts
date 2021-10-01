9fffa8f655e65e8bfdf39caf2f51c205bbcc0d69
christophe <christophegueroult@gmail.com>
2021-10-01 12:01:03 +0200

    pipe async

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from 'src/app/core/services/orders.service';
@@ -11,12 +12,15 @@ import { OrdersService } from 'src/app/core/services/orders.service';
 export class PageListOrdersComponent implements OnInit {
   public title = 'List orders';
   public headers: string[];
-  public collection!: Order[];
+  // public collection!: Order[];
+  public collection$: Observable<Order[]>;
   public states = Object.values(StateOrder);
+  // private sub: Subscription;
   constructor(private ordersService: OrdersService) {
-    this.ordersService.collection.subscribe((data) => {
-      this.collection = data;
-    });
+    this.collection$ = this.ordersService.collection;
+    // this.sub = this.ordersService.collection.subscribe((data) => {
+    //   this.collection = data;
+    // });
     this.headers = [
       'Type',
       'Name',
@@ -37,4 +41,7 @@ export class PageListOrdersComponent implements OnInit {
       Object.assign(item, res);
     });
   }
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 }

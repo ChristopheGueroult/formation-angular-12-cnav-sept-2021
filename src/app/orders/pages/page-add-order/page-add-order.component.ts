36e15c46e74845241f50f8887ecca2f3c440ce50
christophe <christophegueroult@gmail.com>
2021-10-01 15:28:50 +0200

    reactive forms module, validators and @Output()

--- src/app/orders/pages/page-add-order/page-add-order.component.ts
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Order } from 'src/app/core/models/order';
+import { OrdersService } from 'src/app/core/services/orders.service';
 
 @Component({
   selector: 'app-page-add-order',
   templateUrl: './page-add-order.component.html',
-  styleUrls: ['./page-add-order.component.scss']
+  styleUrls: ['./page-add-order.component.scss'],
 })
 export class PageAddOrderComponent implements OnInit {
+  constructor(
+    private ordersService: OrdersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  action(item: Order): void {
+    this.ordersService.add(item).subscribe((res) => {
+      // this.router.navigate(['orders']);
+      this.router.navigate(['../'], { relativeTo: this.route });
+    });
   }
-
 }
